f7d8e50a7e43f645e5a3c682547a04d93f58d3ca
Mahesh Tupe <tupemahesh91@gmail.com>
2023-06-21 00:04:52 +0530

    Initial commit

--- README.rst
+++ README.rst
@@ -1,8 +1,6 @@
-An example Timeflux plugin
+Timeflux plugin for serial data
 ==========================
 
-This is an example plugin that provides a few simple demonstration nodes. Use it as a template
-to develop your own plugins.
 
 Installation
 ------------
@@ -14,4 +12,4 @@ You can then install this plugin in the `timeflux` environment:
 ::
 
     $ conda activate timeflux
-    $ pip install timeflux_example
+    $ pip install timeflux_UDL
---
+++ timeflux_UDL/nodes/driver.py
@@ -0,0 +1,169 @@
+import serial
+import time
+import json
+from json import JSONDecodeError
+from timeflux.core.node import Node
+from timeflux.helpers.clock import now
+from threading import Thread, Lock
+from time import sleep, time
+from pylsl import StreamInfo, StreamOutlet, local_clock
+import numpy as np
+from datetime import timezone
+import datetime
+
+maxFrequency = 2000
+createStream = True
+
+class UDL(Node):
+    """UDL driver.
+
+    Attributes:
+        o (Port): Default output, provides DataFrame.
+
+    Args:
+        port (string): The serial port.
+            e.g. ``COM3`` on Windows;  ``/dev/cu.usbmodem14601`` on MacOS;
+            ``/dev/ttyUSB0`` on GNU/Linux.
+        
+        rate (int): The device rate in Hz.Default: 250. Max limit: 2000 Hz
+
+        channels (int): The number of channels to enable. Default:1.
+        
+        names (list of string): The names of channels. Ex: ['C1', 'C2' ..]
+            Default: Channels are numbered from 1. 
+        
+        debug (bool): If ``True``, print debug information. Default: ``False``.
+
+    Example:
+        .. literalinclude:: /../examples/simple.yaml
+           :language: yaml
+
+    """
+    def __init__(self, port, rate=250, channels=1, names=None, debug=False):
+
+        # Validate input
+        if rate > maxFrequency:
+            raise ValueError(
+                f"'{rate}' exceeded max limit"
+            )
+        
+         # Set channel names
+        if isinstance(names, list) and len(names) == channels:
+            self.names = names
+        else:
+            index = list(range(1, channels + 1))
+            name = []
+            for i in index:
+                name.append("ch_" + i)
+            self.names = name
+
+
+        global serialdevice
+        serialdevice = serial.Serial(port=port, baudrate='115200')
+
+        if (createStream):       
+            data = StreamInfo('udl', 'EEG', channels, rate, 'float32', 'oric6ch')
+            data.desc().append_child_value("manufacturer", "udl")
+            channels = data.desc().append_child("channels")
+            for name in self.names:
+                channels.append_child("channel")\
+                    .append_child_value("label", name)\
+                    .append_child_value("unit", "microvolts")\
+                    .append_child_value("type", "EEG")
+            global outlet
+            outlet = StreamOutlet(data)
+
+        # Compute time offset
+        row = self._read()
+
+        # Remember sample count
+        self._count = 0
+        self._missed = 0
+
+        # Set meta
+        self.meta = {"rate": rate}
+
+        # Launch background thread
+        self._reset()
+        self._lock = Lock()
+        self._running = True
+        self._thread = Thread(target=self._loop).start()
+    
+    def _reset(self):
+        """Empty cache."""
+        self._rows = []
+        self._timestamps = []
+
+    def _loop(self):
+        """Acquire and cache data."""
+        while self._running:
+
+            try:
+                row = self._read()
+                if len(row):
+                    self._check(row[0])
+                    dt = datetime.datetime.now(timezone.utc)
+                    utc_time = dt.replace(tzinfo=timezone.utc)
+                    timestamp = utc_time.timestamp()
+
+                    self._lock.acquire()  # `with self.lock:` is about twice as slow
+                    self._timestamps.append(timestamp)
+                    self._rows.append(row[2])
+                    self._lock.release()
+
+                    if(createStream):
+                        outlet.push_sample(row[2], local_clock())
+            except:
+                pass
+    
+    def _read(self):
+        """Read a line of data from the device."""
+        row = []
+        global serialdevice
+        serialData = serialdevice.readline().rstrip()
+
+        try:
+            response_obj = json.loads(serialData)
+        except JSONDecodeError:
+            response_obj = {}
+            print(f"json decode error: {serialData}")
+            serialdevice.flush()
+    
+        if response_obj:
+            data = list(response_obj.values())
+            packet = data[0]
+            timestamp = data[1]
+            lsldata = data[2:]
+            if lsldata:
+                row = [ packet, timestamp, lsldata]
+        return row
+    
+    def _check(self, count):
+        """Report dropped samples.
+
+        We don't even bother about a possible overflow of the sample counter as it
+        would take about 3 days at 16K SPS before reaching the maximum value.
+        """
+        missed = (count - self._count) - 1
+        self._count = count
+        if missed:
+            self._missed += missed
+        else:
+            if self._missed:
+                self.logger.warn(f"Missed {self._missed} samples")
+                self._missed = 0
+
+    def update(self):
+        """Update the node output."""
+        with self._lock:
+            if self._rows:          
+                self.o.set(self._rows, self._timestamps, self.names, self.meta)
+                self._reset()
+            
+
+    def terminate(self):
+        """Cleanup."""
+        self._running = False
+        while self._thread and self._thread.is_alive():
+            sleep(0.001)
+        
